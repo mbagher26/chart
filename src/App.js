daa9169759ca8b3b98f18500a3535ca1f1ff051c
unknown <mohammadbaghermollanory@gmail.com>
2024-07-03 22:37:37 +0330

    create data state & import Table and Chart components

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
 import './App.css';
+import { useState } from 'react';
+import Chart from './components/chart/chart';
+import Tabel from './components/table/table';
 
 function App() {
+
+  const [data, setData] = useState([]);
+  
   return (
     <div className="App">
+      <Tabel setData={setData}/>
+      <Chart data={data}/>
     </div>
   );
 }
